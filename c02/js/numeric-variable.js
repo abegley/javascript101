fe67439985f70020414eabc6ef1e81e773d420d7
begleyalan <begleyalan@gmail.com>
2015-05-27 20:56:29 +0100

    Basic javascript to calculate cost of each tile.

---
+++ c02/js/numeric-variable.js
@@ -0,0 +1,14 @@
+/**
+ * Created by alan on 27/05/15.
+ */
+
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+var el =document.getElementById('cost');
+el.textContent = '$' + total;
