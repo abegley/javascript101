9623de4e99d0a660ed7b579926d2fcda5465c20b
begleyalan <begleyalan@gmail.com>
2015-05-28 20:49:34 +0100

    Basic JavaScript variable usage

---
+++ c02/js/string-variable.js
@@ -0,0 +1,12 @@
+/**
+ * Created by alan on 28/05/15.
+ */
+var username;
+var message;
+username = 'Alan';
+message = 'All work and no play...';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
